1cb779a32383c648759d83e34c196787b0cb33df
fawad <fbutt7320@gmail.com>
2025-07-23 17:41:58 +0500

    practice 1to5

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1446567
+let accountEmail = "fbutt7320@gmail.com"
+var accountPassowrd = "12345"
+ accountCity = "lahore"
+let accountState;
+
+// accountId = 2
+
+
+accountEmail = "f6@gmail.com"
+accountPassowrd = "54321"
+accountCity = "karachi"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassowrd, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+console.log(3 + 3);
+
+console.log("fawad");
+
+
+let name = "fawad"
+let age = 18
+let isloggedIn = false
+
+
+// number =>2 to power  53
+// bigint
+// srting =>2 ""
+// boolean => true/false
+// null => standalone value
+// undifined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined);//undifined
+console.log(typeof null);
+
+
+
