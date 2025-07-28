68f4a7cf62216bba6da3eb7188250bb0d3052c99
fawad <fbutt7320@gmail.com>
2025-07-28 19:15:29 +0500

    practice 27 to 30

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for(const key in myObject){
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}              
+
+const programming = ["js", "rb", "py", "java", "c++"]
+
+for(const key in programming){
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map();
+// map.set('IN', "India")
+// map.set('pak', "Pakistan")
+// map.set('usa', "UnitedSates")
+// map.set('fr', "France")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
